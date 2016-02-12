2f8224fd1f64cc4b2727280fea8b24142b008256
rileyharrison <rileyharrison@gmail.com>
2016-02-12 15:47:28 -0600

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,34 +1,12 @@
-function functionA(a){
-	var x = a;
-	x = x * 2;
-	return x;
+var avengersMovie = {
+    name:"Avengers",
+    runtime:"143 mins",
+    releaseYear:"2012",
+    director: "Joss Whedon"
+};
+
+
+function logObject(){
+	console.log(avengersMovie);
 }
 
-function functionB(b){
-	var y = b;
-	y = y * 10;
-	return y;
-}
-
-function functionC(c){
-	var z = c;
-	z = z * 100;
-	return z;
-}
-
-console.log(" functionA multiplies argument by 2.");
-console.log(" functionB multiplies argument by 10.");
-console.log(" functionC multiplies argument by 100.");
-console.log(" nested functions are invoked on page load, with 3 passed to functionC, the result passed to functionB, and that passed to functionA.")
-console.log(" for jollys the button click also calls this nested function and changes the button text to the result.")
-
-console.log(functionA(functionB(functionC(3))));
-
-function clickMe(){
-
-	
-
-	var x = functionA(functionB(functionC(3)));
-
-	document.getElementById("myClick").value = x;
-}
